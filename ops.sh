b68ee1b2760f000a23429612f1cbdfc24bdfbe83
nguyenuyen1311 <nguyenuyen19941@gmail.com>
2025-04-08 23:17:33 +0700

    m

--- ops.sh
+++ ops.sh
@@ -1,29 +1,9 @@
 #!/bin/bash
+start_date="2023-01-01 00:00:00"  # Ngày bắt đầu
+seconds_per_commit=86400  # Mỗi commit cách nhau 1 ngày (hoặc tùy chỉnh)
 
-# Number of commits to generate
-NUM_COMMITS=12
-
-# Start date (modify as needed)
-START_DATE="2025-3-10T21:17:35+07:00"
-
-# Convert start date to seconds
-CURRENT_DATE=$(date -d "$START_DATE" +%s)
-
-# Generate random commits
-for ((i=1; i<=NUM_COMMITS; i++)); do
-    # Generate a random time interval between 1 to 24 hours
-    RANDOM_TIME=$((RANDOM % 86400 + 1))  # 1 to 86400 seconds (1 to 24 hours)
-
-    # Update the date
-    CURRENT_DATE=$((CURRENT_DATE + RANDOM_TIME))
-    FORMATTED_DATE=$(date -d "@$CURRENT_DATE" --iso-8601=seconds)
-
-    # Make a commit
-    echo "Random commit $i" > file.txt
-    git add file.txt
-    GIT_COMMITTER_DATE="$FORMATTED_DATE" git commit -m "Cext " --date="$FORMATTED_DATE"
-
-    echo "Committed at: $FORMATTED_DATE"
-done
-git push origin main
+git filter-repo --commit-callback '
+  commit.new_committer_date = int("'"$(date -d "$start_date" +%s)"'") + (commit.original_id * '"$seconds_per_commit"')
+  commit.new_author_date = commit.new_committer_date
+'
 
